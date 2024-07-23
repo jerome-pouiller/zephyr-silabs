304b85b561f6705b1bacfa7954da664f1d418398
Jérôme Pouiller <jerome.pouiller@silabs.com>
2024-07-23 10:47:43 +0200

    Introduce infra for SiWx917 Wifi driver

---
+++ drivers/wifi/siwx917/siwx917_wifi.c
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2023 Antmicro
+ * Copyright (c) 2024 Silicon Laboratories Inc.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#define DT_DRV_COMPAT silabs_siwx917_wifi
+
+#include <zephyr/net/offloaded_netdev.h>
+#include <zephyr/net/wifi_mgmt.h>
+
+static void siwx917_iface_init(struct net_if *iface)
+{
+}
+
+static int siwx917_dev_init(const struct device *dev)
+{
+	return 0;
+}
+
+static enum offloaded_net_if_types siwx917_get_type(void)
+{
+	return L2_OFFLOADED_NET_IF_TYPE_WIFI;
+}
+
+static const struct wifi_mgmt_ops siwx917_mgmt = {
+};
+
+static const struct net_wifi_mgmt_offload siwx917_api = {
+	.wifi_iface.iface_api.init = siwx917_iface_init,
+	.wifi_iface.get_type = siwx917_get_type,
+	.wifi_mgmt_api = &siwx917_mgmt,
+};
+
+NET_DEVICE_DT_INST_OFFLOAD_DEFINE(0, siwx917_dev_init, NULL, NULL, NULL,
+				  CONFIG_WIFI_INIT_PRIORITY, &siwx917_api,
+				  2304);
+
+/* IRQn 74 is used for communication with co-processor */
+Z_ISR_DECLARE(74, ISR_FLAG_DIRECT, IRQ074_Handler, 0);
